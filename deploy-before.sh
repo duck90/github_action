9443c8a210e560ed4297cff3494f5c5e8ef402b4
Donald <duck90.kim@gmail.com>
2023-04-17 18:41:13 +0900

    CHORE: 수정

--- deploy-before.sh
+++ deploy-before.sh
@@ -1,4 +1,4 @@
-if [ -d /home/ec2-user/github_action_test ]; then
+if [ -d /home/ubuntu/github_action_test ]; then
     sudo rm -rf /home/ubuntu/github_action_test/
 fi
 sudo mkdir -vp /home/ubuntu/github_action_test/
