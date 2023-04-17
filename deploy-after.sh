4c3142640992a4b217ed7122045c59f42c63d434
Donald <duck90.kim@gmail.com>
2023-04-17 19:34:56 +0900

    chore: start script 수정

--- deploy-after.sh
+++ deploy-after.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 REPOSITORY=/home/ubuntu/github_action_test
 
-sudo pm2 restart react-app
+cd $REPOSITORY
+
+sudo pm2 start server.js -n react-app
--- deploy-before.sh
+++ deploy-before.sh
@@ -1,4 +1,5 @@
 if [ -d /home/ubuntu/github_action_test ]; then
+    pm2 kill -n react-app
     sudo rm -rf /home/ubuntu/github_action_test/
 fi
 sudo mkdir -vp /home/ubuntu/github_action_test/
