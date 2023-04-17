3a529c54d6471cb186bb1444919170eacf34927b
Donald <duck90.kim@gmail.com>
2023-04-17 18:38:19 +0900

    chore: 배포 스크립트 수정

--- deploy-before.sh
+++ deploy-before.sh
@@ -1,4 +1,4 @@
-if [ -d /home/ec2-user/build ]; then
+if [ -d /home/ec2-user/github_action_test ]; then
     sudo rm -rf /home/ubuntu/github_action_test/
 fi
 sudo mkdir -vp /home/ubuntu/github_action_test/
